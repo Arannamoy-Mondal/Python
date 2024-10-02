192104e8e4e8d3ee70b7d4b39632a7f4997ba9ea
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-10-02 22:37:36 +0600

    Function

---
+++ 8_function.py
@@ -0,0 +1,6 @@
+def sum(num1,num2):
+    return num1+num2
+
+num1=int(input('Enter number:'))
+num2=int(input('Enter number:'))
+print(sum(num1,num2))
---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+       String x=details();
+        System.out.println(details());
+    }
+    public static String details(){
+        return "CPU"+"500"+"Price"+500;
+    }
+} 
