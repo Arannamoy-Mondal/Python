f734e2466dfb3256b8cab6926c6919608a340e78
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-10-09 16:18:51 +0600

    Upd

--- 30_make_a_shopping_system_for_customer_in_a_shopping_mall.py
+++ 30_make_a_shopping_system_for_customer_in_a_shopping_mall.py
@@ -22,16 +22,28 @@ class Shop:
     def product_stock(self):
         for it in self.products:
             print(f'Name: {it.name}, Id: {it.id}, Price: {it.price}, Total Stock: {self.stock[it.name]}')
+    def buy_product(self,name,number):
+        if name in self.products_name and self.stock[name]>=number:
+            print('Congratulations you have successfully bought this product')
+            self.stock[name]-=number
+        else:
+            print('Not Available')
 shop1=Shop()
 while(True):
-    op=int(input('Enter 1 for add an item in cart, 2 for stock, 3 for exit:'))
+    op=int(input('Enter 1 for add an item in cart, 2 for buy product, 3 for exit:'))
     if op==1:
         name=input('Enter name of the product:')
         count=int(input('Number of product:'))
-        tmp=Product(name,count)
-        shop1.add_to_cart(tmp)
+        if(type(name)==str and type(count)==int):
+               tmp=Product(name,count)
+               shop1.add_to_cart(tmp)
+               shop1.product_stock()
     elif op==2:
-        shop1.product_stock()
+        product=input('Enter product name:')
+        number=int(input('Enter number of product'))
+        if(type(name)==str and type(count)==int):
+                shop1.buy_product(product,number)
+                shop1.product_stock()
     elif op==3:
         break
     else:
